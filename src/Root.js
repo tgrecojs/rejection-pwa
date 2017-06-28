112c0fb33d789c0c3eb7b1b26c61fcc6da0f4761
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-06-28 15:10:51 -0400

    added 404 page and changed browserHistory to hashHistory in hopes of gh-pages PWA working

--- src/Root.js
+++ src/Root.js
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import App from './App';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import SignInForm from './user/SignIn'
 import Profile from './user/Profile'
 // import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SignUpForm  from './user/SignUp'
 import QuestionsBase from './questions/QuestionList';
 import NewQuestion from './questions/QuestionForm';
+import PageNotFound from './PageNotFound';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path='/' component={App}>
+      <Route path='*' component={PageNotFound} />
       <IndexRoute component={QuestionsBase} />
       <Route path='login' component={SignInForm} />
       <Route path='register' component={SignUpForm} />
